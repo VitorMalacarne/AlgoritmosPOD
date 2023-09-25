2e268e0718238cd66140ce955fb75612e148b03d
VitorMalacarne <vitorhenriquewm@gmail.com>
2023-09-25 12:34:07 -0300

    .

--- Algoritmos/7_radix_sort.c
+++ Algoritmos/7_radix_sort.c
@@ -1,19 +1,33 @@
-void radixSort(int vetor[], int tam){
+#include <math.h>
+void radixSort(int vetor[], int tamanho)
+{
+    struct timeval inicio, fim;
+    gettimeofday(&inicio, NULL);
+
     int base = 10, r;
-    int digito = 3;//numero de digitos
-    int B[tam], C[base];
-    for(int m = 0; m < digito; m++){
-        for(int i = 0; i < base; ++i) C[i] = 0;
-        for(int j = 0; j < tam; ++j){
-            r = (vetor[j] / (int)(pow(10.0,m))) % base;
-            ++C[r];
+    int digito = 3; // numero de digitos
+    int vetorauxiliar1[tamanho], vetorauxiliar2[base];
+    for (int m = 0; m < digito; m++)
+    {
+        for (int i = 0; i < base; ++i)
+            vetorauxiliar2[i] = 0;
+        for (int j = 0; j < tamanho; ++j)
+        {
+            r = (vetor[j] / (int)(pow(10.0, m))) % base;
+            ++vetorauxiliar2[r];
         }
-        for(int i = 1; i < base; ++i) C[i] += C[i-1];
-        for(int j = tam-1; j >= 0; --j){
-                r = (vetor[j] / (int)(pow(10.0,m))) % base;
-                int i = --C[r];
-                B[i] = vetor[j];
+        for (int i = 1; i < base; ++i)
+            vetorauxiliar2[i] += vetorauxiliar2[i - 1];
+        for (int j = tamanho - 1; j >= 0; --j)
+        {
+            r = (vetor[j] / (int)(pow(10.0, m))) % base;
+            int i = --vetorauxiliar2[r];
+            vetorauxiliar1[i] = vetor[j];
         }
-        for(int j = 0; j < tam; ++j) vetor[j] = B[j];
+        for (int j = 0; j < tamanho; ++j)
+            vetor[j] = vetorauxiliar1[j];
     }
+    gettimeofday(&fim, NULL);
+    double tempo = (fim.tv_sec - inicio.tv_sec) + (fim.tv_usec - inicio.tv_usec) / 1000000.0;
+    printf("Tempo de Ordenação do arquivo com %d dados: %.5f\n", tamanho, tempo);
 }
