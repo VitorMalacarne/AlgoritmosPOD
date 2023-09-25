2d59ba0f52dc9b6b83dcf18b6820ecce8962afbb
VitorMalacarne <vitorhenriquewm@gmail.com>
2023-09-25 14:39:31 -0300

    funcionando

--- Algoritmos/5.2_quick_sort_lomuto.c
+++ Algoritmos/5.2_quick_sort_lomuto.c
@@ -1,40 +1,36 @@
 int particionelomuto(int vetor[], int esquerda, int direita)
 {
-  int pivo = vetor[esquerda], i = esquerda, j = esquerda + 1;
+    int pivo = vetor[direita];
+    int i = esquerda -1;
 
-  while (j < direita)
-  {
-    while (vetor[j] > pivo)
-    {
-      j++;
+    for (int j = esquerda; j <= direita - 1; j++) {
+        if (vetor[j] < pivo) {
+            i++;
+            troca(vetor, i, j);
+        }
     }
-    i++;
-    troca(vetor, i, j);
-  }
-  troca(vetor, i, pivo);
-
-  return i;
+    troca(vetor, i+1, direita);
+    return (i + 1);
 }
 
 void quicksortlomutorecursivo(int vetor[], int esquerda, int direita)
 {
-  int i;
-  if (direita > esquerda)
-  {
-    i = particionelomuto(vetor, esquerda, direita);
-    quicksortlomutorecursivo(vetor, esquerda, i - 1);
-    quicksortlomutorecursivo(vetor, i + 1, direita);
-  }
+    int i;
+    if (direita > esquerda)
+    {
+        i = particionelomuto(vetor, esquerda, direita);
+        quicksortlomutorecursivo(vetor, esquerda, i - 1);
+        quicksortlomutorecursivo(vetor, i + 1, direita);
+    }
 }
 
-void quicksortlomuto(int vetor[], int tamanho)
+void quicksort(int vetor[], int tamanho)
 {
-  struct timeval inicio, fim;
-  gettimeofday(&inicio, NULL);
+    struct timeval inicio, fim;
+    gettimeofday(&inicio, NULL);
+    quicksortlomutorecursivo(vetor, 0, tamanho);
 
-  quicksortlomutorecursivo(vetor, 0, tamanho);
-
-  gettimeofday(&fim, NULL);
-  double tempo = (fim.tv_sec - inicio.tv_sec) + (fim.tv_usec - inicio.tv_usec) / 1000000.0;
-  printf("Tempo de Ordenação do arquivo com %d dados: %.5f\n", tamanho, tempo);
+    gettimeofday(&fim, NULL);
+    double tempo = (fim.tv_sec - inicio.tv_sec) + (fim.tv_usec - inicio.tv_usec) / 1000000.0;
+    printf("Tempo de Ordenação do arquivo com %d dados: %.5f\n", tamanho, tempo);
 }
--- programa.c
+++ programa.c
@@ -2,41 +2,37 @@
 
 int main(void)
 {
-  int vetor[1000];
-  system("clear");
-  printf("Merge Sort\n");
-  printf("\nVetor Aleatório:\n");
-  lerArquivo(vetor, "output/Aleatorio500.txt");
-  radixSort(vetor, 500);
-  imprimevetor(vetor, 500);
-  lerArquivo(vetor, "output/Aleatorio750.txt");
-  radixSort(vetor, 750);
-  // imprimevetor(vetor, 750);
-  lerArquivo(vetor, "output/Aleatorio1000.txt");
-  radixSort(vetor, 1000);
-  // imprimevetor(vetor, 1000);
+  int vetor[500];
+    system("clear");
+    printf("Quick Sort Lomuto\n");
 
-  printf("\nVetor Decrescente:\n");
-  lerArquivo(vetor, "output/Decrescente500.txt");
-  radixSort(vetor, 500);
-  // imprimevetor(vetor, 500);
-  lerArquivo(vetor, "output/Decrescente750.txt");
-  radixSort(vetor, 750);
-  // imprimevetor(vetor, 750);
-  lerArquivo(vetor, "output/Decrescente1000.txt");
-  radixSort(vetor, 1000);
-  // imprimevetor(vetor, 1000);
+    printf("\nVetor Aleatório:\n");
+    lerArquivo(vetor, "output/Aleatorio500.txt");
+    imprimevetor(vetor, 500);
+    quicksort(vetor,500);
+    imprimevetor(vetor, 500);
+    /*
+    lerArquivo(vetor, "Aleatorio750.txt");
+    quicksortlomuto(vetor, 750);
+    lerArquivo(vetor, "Aleatorio1000.txt");
+    quicksortlomuto(vetor, 1000);
 
-  printf("\nVetor Ordenado:\n");
-  lerArquivo(vetor, "output/Ordenado500.txt");
-  radixSort(vetor, 500);
-  // imprimevetor(vetor, 500);
-  lerArquivo(vetor, "output/Ordenado750.txt");
-  radixSort(vetor, 750);
-  // imprimevetor(vetor, 750);
-  lerArquivo(vetor, "output/Ordenado1000.txt");
-  radixSort(vetor, 1000);
-  // imprimevetor(vetor, 1000);
+    printf("\nVetor Decrescente:\n");
+    lerArquivo(vetor, "Decrescente500.txt");
+    quicksortlomuto(vetor, 500);
+    lerArquivo(vetor, "Decrescente750.txt");
+    quicksortlomuto(vetor, 750);
+    lerArquivo(vetor, "Decrescente1000.txt");
+    quicksortlomuto(vetor, 1000);
+
+    printf("\nVetor Ordenado:\n");
+    lerArquivo(vetor, "Ordenado500.txt");
+    quicksortlomuto(vetor, 500);
+    lerArquivo(vetor, "Ordenado750.txt");
+    quicksortlomuto(vetor, 750);
+    lerArquivo(vetor, "Ordenado1000.txt");
+    quicksortlomuto(vetor, 1000);
+    espera();*/
   espera();
   return 0;
 }
